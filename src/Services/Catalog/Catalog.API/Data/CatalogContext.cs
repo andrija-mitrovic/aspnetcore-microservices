4c37d288ad130440a8059e07224693b22260f6a1
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-02 09:24:43 +0200

    Add catalog db context

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration, IOptions<MongoSettings> mongo)
+        {
+            //var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            //var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            //Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            var client = new MongoClient(mongo.Value.ConnectionString);
+            var database = client.GetDatabase(mongo.Value.DatabaseName);
+
+            Products = database.GetCollection<Product>(mongo.Value.CollectionName);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
