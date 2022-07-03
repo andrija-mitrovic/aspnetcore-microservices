7fc733273aabeb0bf1ef4a4dddbbc22966d05ba4
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-03 08:05:04 +0200

    Add helper function

---
+++ src/Services/Catalog/Catalog.API/Helpers/HelperFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Catalog.API.Helpers
+{
+    public static class HelperFunctions
+    {
+        public static string GetMethodName([CallerMemberName] string memberName = "")
+        {
+            return memberName;
+        }
+    }
+}
