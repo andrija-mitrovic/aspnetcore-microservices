c2ebbacc4712ccb072ed085fe292f669aa4e0bf7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-06 08:25:40 +0200

    Add serilog settings model

---
+++ src/Services/Catalog/Catalog.API/Models/SerilogSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Models
+{
+    public class SerilogSettings
+    {
+        public const string SectionName = "Serilog";
+
+        [ConfigurationKeyName("MinimumLevel")]
+        public MinimumLevel MinLevel { get; set; } = null!;
+        public ElasticSearchSettings ElasticSearch { get; set; } = null!;
+
+        public class ElasticSearchSettings
+        {
+            public string Uri { get; set; } = null!;
+        }
+
+        public class MinimumLevel
+        {
+            public string Default { get; set; } = null!;
+        }
+    }
+}
