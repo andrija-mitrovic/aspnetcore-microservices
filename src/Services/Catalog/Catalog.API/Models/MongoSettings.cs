90b24602504d4f0da2af51bce9deb565ad26c3cf
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-06 08:25:27 +0200

    Add mongo settings model

---
+++ src/Services/Catalog/Catalog.API/Models/MongoSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models
+{
+    
+    public class MongoSettings
+    {
+        public const string SectionName = "DatabaseSettings";
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get;set; } = null!;
+    }
+}
