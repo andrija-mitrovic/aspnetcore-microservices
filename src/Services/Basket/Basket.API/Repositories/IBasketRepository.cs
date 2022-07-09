5aac22ec82a85b700eeed543a097107412531005
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-09 08:49:07 +0200

    Add basket repository

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        public Task<ShoppingCart?> GetBasket(string userName);
+        public Task<ShoppingCart?> UpdateBasket(ShoppingCart basket);
+        public Task DeleteBasket(string userName);
+    }
+}
