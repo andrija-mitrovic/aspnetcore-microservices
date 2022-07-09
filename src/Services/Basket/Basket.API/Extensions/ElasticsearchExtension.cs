a8edea7c7154b4c4526cd59bdbfb6a99e90c069c
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-09 08:49:22 +0200

    Add elasticsearch

---
+++ src/Services/Basket/Basket.API/Extensions/ElasticsearchExtension.cs
@@ -0,0 +1,61 @@
+﻿using Basket.API.Constants;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
+namespace Basket.API.Extensions
+{
+    public static class ElasticsearchExtension
+    {
+        public static void AddElasticsearch(this WebApplicationBuilder builder)
+        {
+            LoggerConfigure();
+
+            builder.Host.UseSerilog();
+        }
+
+        private static void LoggerConfigure()
+        {
+            var environmentName = Environment.GetEnvironmentVariable(AppConstants.ENVIRONMENT);
+            IConfigurationRoot configuration = ReturnConfigurationRoot(environmentName!);
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(environmentName!, configuration))
+                .Enrich.WithProperty("Environment", environmentName)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static IConfigurationRoot ReturnConfigurationRoot(string environmentName)
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true)
+                .Build();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(string environmentName, IConfiguration configuration)
+        {
+            var minimumLogLevel = configuration.GetValue<string>(AppConstants.ELASTIC_SEARCH_LOG_LEVEL);
+            var esUri = configuration.GetValue<string>(AppConstants.ELASTIC_SEARCH_URI);
+
+            _ = Enum.TryParse(minimumLogLevel, out LogEventLevel logEventLevel);
+
+            return new ElasticsearchSinkOptions(new Uri(esUri))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = ReturnIndexFormat(environmentName),
+                MinimumLogEventLevel = logEventLevel
+            };
+        }
+
+        private static string ReturnIndexFormat(string environmentName) =>
+            $"{Assembly.GetExecutingAssembly().GetName().Name?.ToLower().Replace(".", "-")}-" +
+            $"{environmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}";
+    }
+}
