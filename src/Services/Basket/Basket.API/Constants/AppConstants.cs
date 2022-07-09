c7be309087b456d98214d8d7244051a503f409cd
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-09 08:48:51 +0200

    Add class for storing global constants inside basket api project

---
+++ src/Services/Basket/Basket.API/Constants/AppConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Constants
+{
+    public class AppConstants
+    {
+        public const string ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";
+        public const string GRPC_DISCOUNT_SETTINGS = "GrpcSettings:DiscountUrl";
+        public const string ELASTIC_SEARCH_LOG_LEVEL = "Serilog:MinimumLevel:Default";
+        public const string ELASTIC_SEARCH_URI = "Serilog:ElasticSearch:Uri";
+        public const string REDIS_CONNECTION_STRING = "CacheSettings:ConnectionString";
+        public const string EVENT_BUS_HOST_ADDRESS = "EventBusSettings:HostAddress";
+    }
+}
