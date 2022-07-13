198d878edf1cc1d59f0a883c5a1a442f0ac2c696
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-13 09:03:16 +0200

    Add class to configure all services at one place

---
+++ src/Services/Basket/Basket.API/ConfigureServices.cs
@@ -0,0 +1,51 @@
+﻿using Basket.API.Constants;
+using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+using Discount.Grpc.Protos;
+using MassTransit;
+
+namespace Basket.API
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddBasketAPIServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Redis Configuration
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration[AppConstants.REDIS_CONNECTION_STRING];
+            });
+
+            //Grpc Configuration
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (opt => opt.Address = new Uri(configuration[AppConstants.GRPC_DISCOUNT_SETTINGS]));
+
+            services.AddControllers();
+
+            //General Configuration
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddScoped<IDiscountGrpcService, DiscountGrpcService>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            //MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(configuration[AppConstants.EVENT_BUS_HOST_ADDRESS]);
+                });
+            });
+
+            services.AddMassTransitHostedService();
+
+            services.AddHealthChecks().AddRedis(configuration[AppConstants.REDIS_CONNECTION_STRING], name: "Redis");
+
+            return services;
+        }
+    }
+}
