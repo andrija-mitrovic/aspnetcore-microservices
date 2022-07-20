b114acadf8f577b9a918d8845b3f84962ad0a9be
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-20 08:20:22 +0200

    Add global configuration service class for discount api

---
+++ src/Services/Discount/Discount.API/ConfigureServices.cs
@@ -0,0 +1,23 @@
+﻿using Discount.API.Constants;
+using Discount.API.Repositories;
+
+namespace Discount.API
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddDiscountAPIServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddHealthChecks().AddNpgSql(configuration[AppConstants.POSTGRES_DATABASE_CONNECTION], name: "PostgreSql");
+
+            return services;
+        }
+    }
+}
