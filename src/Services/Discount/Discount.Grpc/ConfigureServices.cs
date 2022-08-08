580a495dd7b0688e022ce6ba9ef3bf680f10d124
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-08 08:13:34 +0200

    Configure discount proto file

---
+++ src/Services/Discount/Discount.Grpc/ConfigureServices.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Grpc.Constants;
+using Discount.Grpc.Repositories;
+
+namespace Discount.Grpc
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddDiscountGrpcServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddGrpc();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+            return services;
+        }
+    }
+}
