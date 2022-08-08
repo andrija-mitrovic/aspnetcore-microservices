bdd3c9da3e6a13cce58b6c1100c90022142eb53a
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-08 08:12:14 +0200

    Add class for storing global constants

---
+++ src/Services/Discount/Discount.Grpc/Constants/AppConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.Grpc.Constants
+{
+    public class AppConstants
+    {
+        public const string POSTGRES_DATABASE_CONNECTION = "DatabaseSettings:ConnectionString";
+        public const string ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";
+        public const string ELASTIC_SEARCH_LOG_LEVEL = "Serilog:MinimumLevel:Default";
+        public const string ELASTIC_SEARCH_URI = "Serilog:ElasticSearch:Uri";
+    }
+}
