566bc65b6b8ac1806ecd86ca4204bc68a0da11e6
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-10 11:04:25 +0200

    Add email and emailsettings model

---
+++ src/Services/Ordering/Ordering.Application/Common/Models/EmailSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Common.Models
+{
+    public class EmailSettings
+    {
+        public const string SectionName = "EmailSettings";
+        public string ApiKey { get; set; } = null!;
+        public string FromAddress { get; set; } = null!;
+        public string FromName { get; set; } = null!;
+    }
+}
