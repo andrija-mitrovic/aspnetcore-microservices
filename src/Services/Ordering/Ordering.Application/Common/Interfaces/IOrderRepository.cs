013a9be1ea45cfd11e8578d5727b6945b2821f65
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-11 08:40:51 +0200

    Add order respository

---
+++ src/Services/Ordering/Ordering.Application/Common/Interfaces/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Common.Interfaces
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
