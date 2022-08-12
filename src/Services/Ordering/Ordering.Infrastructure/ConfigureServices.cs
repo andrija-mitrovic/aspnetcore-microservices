439c4e0759f52109a3144a0ea3b37d6fd933f3ce
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-12 07:20:18 +0200

    Add configuration service class for infrastructure layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Common.Constants;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Application.Common.Models;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Persistence.Interceptors;
+using Ordering.Infrastructure.Persistence.Repositories;
+using Ordering.Infrastructure.Services;
+
+namespace Ordering.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(AppConstants.MSSQL_DATABASE_CONNECTION)));
+
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+            services.AddScoped<ApplicationDbContextInitialiser>();
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddTransient<IDateTime, DateTimeService>();
+            services.AddTransient<IEmailService, EmailService>();
+
+            services.Configure<EmailSettings>(x => configuration.GetSection(EmailSettings.SectionName));
+
+            return services;
+        }
+    }
+}
