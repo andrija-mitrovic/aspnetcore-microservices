1bc82af9b96c16661ad1cd6d77663e1bb136bd9b
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-12 07:19:34 +0200

    Add application db context

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence.Interceptors;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor) : base(options)
+        {
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        public DbSet<Order> Orders => Set<Order>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+        }
+    }
+}
