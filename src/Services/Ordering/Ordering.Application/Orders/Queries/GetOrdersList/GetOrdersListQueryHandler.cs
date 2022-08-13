1714086b2b9106e1b63fa93798a941112314af69
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-13 06:19:17 +0200

    Add get orders feature

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Common.Interfaces;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrderDto>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrderDto>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrdersByUserName(request.UserName);
+
+            return _mapper.Map<List<OrderDto>>(orders);
+        }
+    }
+}
