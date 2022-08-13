034ecd0491d6b0b4e4a1e2f17f0852420958b5e7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-13 06:18:56 +0200

    Add update order command

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Common.Exceptions;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, 
+            IMapper mapper, 
+            ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (order == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            _mapper.Map(request, order);
+
+            await _orderRepository.UpdateAsync(order);
+
+            _logger.LogInformation("Order {OrderId} is successfully updated.", order.Id);
+
+            return Unit.Value;
+        }
+    }
+}
