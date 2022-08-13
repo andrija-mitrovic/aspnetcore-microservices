994bb7ed6b147bf1337c4cbf805c44806aadbc89
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-13 06:19:45 +0200

    Add class for storing global constants

---
+++ src/Services/Ordering/Ordering.Application/Common/Constants/AppConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Common.Constants
+{
+    public class AppConstants
+    {
+        public const string MSSQL_DATABASE_CONNECTION = "MssqlConnectionString";
+        public const string EVENT_BUS_HOST_ADDRESS = "EventBusSettings:HostAddress";
+        public const string ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";
+        public const string ELASTIC_SEARCH_LOG_LEVEL = "Serilog:MinimumLevel:Default";
+        public const string ELASTIC_SEARCH_URI = "Serilog:ElasticSearch:Uri";
+    }
+}
