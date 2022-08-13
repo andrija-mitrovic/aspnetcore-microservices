e1d6c22b0897312687c9655b1966af6215153985
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-13 06:18:47 +0200

    Add delete order feature

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Common.Exceptions;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, 
+            IMapper mapper, 
+            ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (order == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await _orderRepository.DeleteAsync(order!);
+
+            _logger.LogInformation("Order {OrderId} is successfully deleted.", order.Id);
+
+            return Unit.Value;
+        }
+    }
+}
