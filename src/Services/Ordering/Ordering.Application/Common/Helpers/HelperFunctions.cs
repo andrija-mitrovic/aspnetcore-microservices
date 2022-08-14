7b265038d1d1f5dc636dcdeac4a59be5050154e0
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-14 08:28:10 +0200

    Add helper class

---
+++ src/Services/Ordering/Ordering.Application/Common/Helpers/HelperFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ordering.Application.Common.Helpers
+{
+    public static class HelperFunctions
+    {
+        public static string GetMethodName([CallerMemberName] string memberName = "")
+        {
+            return memberName;
+        }
+    }
+}
