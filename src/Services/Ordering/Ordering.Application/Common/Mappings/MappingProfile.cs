a8dad372bf55512f70f3fb873a618fc59a20b929
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-14 08:27:57 +0200

    Add mapping profile

---
+++ src/Services/Ordering/Ordering.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Ordering.Application.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Orders.Commands.UpdateOrder;
+using Ordering.Application.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
