16980287924110e2f9872175481bd85511c7e28d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-15 08:11:02 +0200

    Add ordering profile

---
+++ src/Services/Ordering/Ordering.API/Mappings/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Event.Messages.Events;
+using Ordering.Application.Orders.Commands.CheckoutOrder;
+
+namespace Ordering.API.Mappings
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
