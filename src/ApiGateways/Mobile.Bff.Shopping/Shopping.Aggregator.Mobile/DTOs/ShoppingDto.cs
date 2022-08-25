493428a7d7783f6a96b45acdc07bd2a094d3b041
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-25 08:07:19 +0200

    Add shopping dto

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/DTOs/ShoppingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Mobile.DTOs
+{
+    public class ShoppingDto
+    {
+        public string? UserName { get; set; }
+        public BasketDto? BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseDto>? Orders { get; set; }
+    }
+}
