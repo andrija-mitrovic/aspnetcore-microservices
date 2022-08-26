b0a1a60ea23d4de903e58070f203359cd5bcfdf9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-26 08:17:15 +0200

    Add ordering model

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Models/Ordering.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Mobile.Models
+{
+    public class Ordering
+    {
+        public string Url { get; set; } = null!;
+        public string BasePath { get; set; } = null!;
+    }
+}
