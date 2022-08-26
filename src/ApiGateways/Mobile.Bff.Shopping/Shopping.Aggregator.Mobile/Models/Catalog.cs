6d9cc01b54d3768ab11d896013bbd1ef8deae9ac
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-26 08:17:28 +0200

    Add catalog model

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Models/Catalog.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Mobile.Models
+{
+    public class Catalog
+    {
+        public string Url { get; set; } = null!;
+        public string BasePath { get; set; } = null!;
+    }
+}
