a8fe5e230fd0c61c0d5398676021d0a530d8231e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-26 08:17:37 +0200

    Add basket model

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Models/Basket.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Mobile.Models
+{
+    public class Basket
+    {
+        public string Url { get; set; } = null!;
+        public string BasePath { get; set; } = null!;
+    }
+}
