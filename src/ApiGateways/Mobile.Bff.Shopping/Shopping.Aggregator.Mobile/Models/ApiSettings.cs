32e999381cf89db52255815130152a4ce2d14ada
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-26 08:17:50 +0200

    Add api settings model

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Models/ApiSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Mobile.Models
+{
+    public class ApiSettings
+    {
+        public static Catalog Catalog { get; set; } = null!;
+        public static Basket Basket { get; set; } = null!;
+        public static Ordering Ordering { get; set; } = null!;
+    }
+}
