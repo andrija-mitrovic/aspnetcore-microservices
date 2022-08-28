0715e4addaad5d0c036ecfead895581b64bde5af
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-28 08:13:13 +0200

    Add order service in mobile shopping aggregator

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Interfaces/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using Shopping.Aggregator.Mobile.DTOs;
+
+namespace Shopping.Aggregator.Mobile.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseDto>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Mobile.DTOs;
+using Shopping.Aggregator.Mobile.Extensions;
+using Shopping.Aggregator.Mobile.Interfaces;
+using Shopping.Aggregator.Mobile.Models;
+
+namespace Shopping.Aggregator.Mobile.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<OrderResponseDto>> GetOrdersByUserName(string userName)
+        {
+            var response = await _httpClient.GetAsync($"{ApiSettings.Ordering.BasePath}/{userName}");
+
+            return await response.ReadContentAs<IEnumerable<OrderResponseDto>>();
+        }
+    }
+}
