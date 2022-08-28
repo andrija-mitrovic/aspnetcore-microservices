a54315c0e502b4842a6a49fa49007b51e039566c
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-28 08:13:45 +0200

    Add catalog service in mobile shopping aggregator

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Interfaces/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Mobile.DTOs;
+
+namespace Shopping.Aggregator.Mobile.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogDto>> GetCatalog();
+        Task<IEnumerable<CatalogDto>> GetCatalogByCategory(string category);
+        Task<CatalogDto> GetCatalog(string id);
+    }
+}
---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Services/CatalogService.cs
@@ -0,0 +1,38 @@
+﻿using Shopping.Aggregator.Mobile.DTOs;
+using Shopping.Aggregator.Mobile.Extensions;
+using Shopping.Aggregator.Mobile.Interfaces;
+using Shopping.Aggregator.Mobile.Models;
+
+namespace Shopping.Aggregator.Mobile.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<CatalogDto>> GetCatalog()
+        {
+            var response = await _httpClient.GetAsync(ApiSettings.Catalog.BasePath);
+
+            return await response.ReadContentAs<IEnumerable<CatalogDto>>();
+        }
+
+        public async Task<CatalogDto> GetCatalog(string id)
+        {
+            var response = await _httpClient.GetAsync($"{ApiSettings.Catalog.BasePath}/{id}");
+
+            return await response.ReadContentAs<CatalogDto>();
+        }
+
+        public async Task<IEnumerable<CatalogDto>> GetCatalogByCategory(string category)
+        {
+            var response = await _httpClient.GetAsync($"{ApiSettings.Catalog.BasePath}/GetProductByCategory/{category}");
+
+            return await response.ReadContentAs<IEnumerable<CatalogDto>>();
+        }
+    }
+}
