d6afaca8b2e9a4c1d70113ac1cdc213b3bf0673f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-28 08:13:27 +0200

    Add basket service in mobile shopping aggregator

---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Interfaces/IBasketService.cs
@@ -0,0 +1,9 @@
+﻿using Shopping.Aggregator.Mobile.DTOs;
+
+namespace Shopping.Aggregator.Mobile.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<BasketDto> GetBasket(string userName);
+    }
+}
---
+++ src/ApiGateways/Mobile.Bff.Shopping/Shopping.Aggregator.Mobile/Services/BasketService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Mobile.DTOs;
+using Shopping.Aggregator.Mobile.Extensions;
+using Shopping.Aggregator.Mobile.Interfaces;
+using Shopping.Aggregator.Mobile.Models;
+
+namespace Shopping.Aggregator.Mobile.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BasketDto> GetBasket(string userName)
+        {
+            var response = await _httpClient.GetAsync($"{ApiSettings.Catalog.BasePath}/{userName}");
+
+            return await response.ReadContentAs<BasketDto>();
+        }
+    }
+}
