9cf45458b3b46184138d54f3de06d3fb62eddab8
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-05 08:16:37 +0100

    Add order service

---
+++ src/ApiGateways/Web.Bff.Shopping/Shopping.Aggregator.Web/Interfaces/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using Shopping.Aggregator.Web.DTOs;
+
+namespace Shopping.Aggregator.Web.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseDto>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ src/ApiGateways/Web.Bff.Shopping/Shopping.Aggregator.Web/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Web.DTOs;
+using Shopping.Aggregator.Web.Extensions;
+using Shopping.Aggregator.Web.Interfaces;
+using Shopping.Aggregator.Web.Models;
+
+namespace Shopping.Aggregator.Web.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<OrderResponseDto>> GetOrdersByUserName(string userName)
+        {
+            var response = await _httpClient.GetAsync($"{ApiSettings.Ordering.BasePath}/{userName}");
+
+            return await response.ReadContentAs<IEnumerable<OrderResponseDto>>();
+        }
+    }
+}
