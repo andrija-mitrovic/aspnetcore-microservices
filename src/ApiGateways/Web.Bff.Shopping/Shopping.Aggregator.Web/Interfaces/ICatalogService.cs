852ece255a5d16b51b19590af989f6a97af4cc1f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-12-06 08:15:53 +0100

    Add catalog service

---
+++ src/ApiGateways/Web.Bff.Shopping/Shopping.Aggregator.Web/Interfaces/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Web.DTOs;
+
+namespace Shopping.Aggregator.Web.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogDto>> GetCatalog();
+        Task<IEnumerable<CatalogDto>> GetCatalogByCategory(string category);
+        Task<CatalogDto> GetCatalog(string id);
+    }
+}
